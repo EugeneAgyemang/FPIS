6a3c1aa10611354d3d15b158f1988976c07171de
Martin Amankwaa <martyofmca@gmail.coms>
2022-12-21 01:51:20 +0000

    Add Startup screen

---
+++ FPIS/Startup.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace FPIS
+{
+    partial class Startup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialDivider1 = new MaterialSkin.Controls.MaterialDivider();
+            this.SuspendLayout();
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.materialLabel1.FontType = MaterialSkin.MaterialSkinManager.fontType.Caption;
+            this.materialLabel1.Location = new System.Drawing.Point(391, 368);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(131, 14);
+            this.materialLabel1.TabIndex = 0;
+            this.materialLabel1.Text = "Powered by ITS Gh. v1.0";
+            this.materialLabel1.UseAccent = true;
+            // 
+            // materialLabel2
+            // 
+            this.materialLabel2.AutoSize = true;
+            this.materialLabel2.Depth = 0;
+            this.materialLabel2.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.materialLabel2.Location = new System.Drawing.Point(308, 191);
+            this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel2.Name = "materialLabel2";
+            this.materialLabel2.Size = new System.Drawing.Size(214, 19);
+            this.materialLabel2.TabIndex = 1;
+            this.materialLabel2.Text = "Finalising incredible settings...";
+            // 
+            // materialDivider1
+            // 
+            this.materialDivider1.BackColor = System.Drawing.Color.Black;
+            this.materialDivider1.Depth = 0;
+            this.materialDivider1.Location = new System.Drawing.Point(-27, 0);
+            this.materialDivider1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialDivider1.Name = "materialDivider1";
+            this.materialDivider1.Size = new System.Drawing.Size(329, 403);
+            this.materialDivider1.TabIndex = 2;
+            this.materialDivider1.Text = "materialDivider1";
+            // 
+            // Startup
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(550, 400);
+            this.Controls.Add(this.materialDivider1);
+            this.Controls.Add(this.materialLabel2);
+            this.Controls.Add(this.materialLabel1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormStyle = MaterialSkin.Controls.MaterialForm.FormStyles.StatusAndActionBar_None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Startup";
+            this.Padding = new System.Windows.Forms.Padding(3, 0, 3, 3);
+            this.Sizable = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel2;
+        private MaterialSkin.Controls.MaterialDivider materialDivider1;
+    }
+}
---
+++ FPIS/Startup.cs
@@ -0,0 +1,33 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using static MaterialSkin.MaterialSkinManager;
+
+namespace FPIS
+{
+    public partial class Startup : MaterialForm
+    {
+        public Startup()
+        {
+            InitializeComponent();
+            SetTheme(Themes.LIGHT);
+        }
+
+        private ColorScheme SetTheme(Themes materialSkinTheme)
+        {
+            MaterialSkinManager skinManager = Instance;
+            skinManager.AddFormToManage(this);
+            switch (materialSkinTheme)
+            {
+                case Themes.LIGHT:
+                    skinManager.ColorScheme = new ColorScheme(Primary.Purple900, Primary.Purple900, Primary.Purple900, Accent.Purple700, TextShade.WHITE);
+                    break;
+                case Themes.DARK:
+                    skinManager.ColorScheme = new ColorScheme(Primary.BlueGrey900, Primary.BlueGrey900, Primary.BlueGrey900, Accent.Green700, TextShade.WHITE);
+                    break;
+            }
+            skinManager.Theme = materialSkinTheme;
+            materialDivider1.BackColor = Color.FromArgb(38, 50, 56);
+            return skinManager.ColorScheme;
+        }
+    }
+}
