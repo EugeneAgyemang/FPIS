337e742faba1d139141a4c6a49cb5936f432263a
EugeneAgyemang <eugeneagyemang.ea@gmail.com>
2022-12-30 19:25:48 +0000

    Hello Guys, The Entity framework core configuration is done

---
+++ FPIS/Models/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FPIS.Models
+{
+    public class AppDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=FPIS;Username=postgres;Password=eugene");
+        }
+
+
+    }
+}
--- FPIS/Program.cs
+++ FPIS/Program.cs
@@ -1,3 +1,5 @@
+using FPIS.Views;
+
 namespace FPIS
 {
     internal static class Program
@@ -11,7 +13,7 @@ namespace FPIS
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Startup());
+            Application.Run(new Main());
         }
     }
 }
