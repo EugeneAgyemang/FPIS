19ee878537ca61bf4307cfcc5854adccf3af5dc8
Martin Amankwaa <martyofmca@gmail.coms>
2023-01-16 01:41:24 +0000

    fix: correct the namespace for Utils.cs
    fix the namespace for the Utils source code file because it was preventing
    the code from compiling. It was ealier in the FPIS namespace.

--- FPIS/Utils/Utils.cs
+++ FPIS/Utils/Utils.cs
@@ -1,25 +1,24 @@
 ﻿
 using System.Security.Cryptography;
 
-namespace FPIS
+namespace FPIS.Utils;
+
+public class Utils
 {
-    public class Utils
+    /// <summary>
+    /// Hashes a string with SHA-512
+    /// </summary>
+    /// <param name="rawPasswordString"></param>
+    /// <returns>string</returns>
+    public static string HashPassword(string rawPasswordString)
     {
-        /// <summary>
-        /// Hashes a string with SHA-512
-        /// </summary>
-        /// <param name="rawPasswordString"></param>
-        /// <returns>string</returns>
-        public static string HashPassword(string rawPasswordString)
-        {
-            // TODO: Implement a true hashing for passwords
+        // TODO: Implement a true hashing for passwords
 
-            string encryptedPassword = rawPasswordString;
-            // HashAlgorithm sha512 = SHA512.Create();
-            // byte[] b = new byte[32];
+        string encryptedPassword = rawPasswordString;
+        // HashAlgorithm sha512 = SHA512.Create();
+        // byte[] b = new byte[32];
 
-            // sha512.ComputeHash(rawPasswordString.ToArray());
-            return $"en_-{encryptedPassword}-_ted";
-        }
+        // sha512.ComputeHash(rawPasswordString.ToArray());
+        return $"en_-{encryptedPassword}-_ted";
     }
 }
