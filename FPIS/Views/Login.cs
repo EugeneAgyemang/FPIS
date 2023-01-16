cc31410c51d84c3b53cba2cab42e4cbba2143545
Martin Amankwaa <martyofmca@gmail.coms>
2023-01-16 02:16:16 +0000

    refactor: cleanup Login.cs with right functions
    made code changes in the AuthenticateUser and UserProvidedData functions
    to suit the changes made. These functions are more modular and perform
    better than earlier because now they only have to do just one thing and
    return some token or value to the caller to decide what to do.

--- FPIS/Views/Login.cs
+++ FPIS/Views/Login.cs
@@ -1,4 +1,5 @@
 ﻿using FPIS.Models;
+using FPIS.Utils;
 using FPIS.Views;
 using MaterialSkin.Controls;
 using static MaterialSkin.MaterialSkinManager;
@@ -19,34 +20,49 @@ namespace FPIS
         {
             string employeeId = txtEmpId.Text;
             string password = txtPassword.Text;
+            LoginAuth authToken = AuthenticateUser(employeeId, password);
+            switch (authToken)
+            {
+                case LoginAuth.NOUSERNAME:
+                    MaterialMessageBox.Show("Kindly provide your Employee ID", "Login Failed");
+                    txtEmpId.Focus();
+                    break;
+                case LoginAuth.NOPASSWORD:
+                    MaterialMessageBox.Show("Kindly provide your password", "Login Failed");
+                    txtPassword.Focus();
+                    break;
+                case LoginAuth.AUTH_FAIL:
+                    MaterialMessageBox.Show("Invalid login credentials", "Login Failed");
+                    // The called function currently only clears the password
+                    ResetFields();
+                    break;
+                case LoginAuth.AUTH_PASS:
+                    MaterialMessageBox.Show("Welcome", "Login Passed");
+                    break;
+            }
+        }
+
+        internal LoginAuth TestAuthenticateUser(string employeeId, string password) => AuthenticateUser(employeeId, password);
+
+        private LoginAuth AuthenticateUser(string employeeId, string password)
+        {
             // Seperately check the username and passwords to show different error messages
             bool dataProvided = UserProvidedData(employeeId);
             if (!(dataProvided))
-            {
-                MaterialMessageBox.Show("Kindly provide your Employee ID", "Login Failed");
-                txtEmpId.Focus();
-                return;
-            }
+                return LoginAuth.NOUSERNAME;
             dataProvided = UserProvidedData(password);
             if (!(dataProvided))
-            {
-                MaterialMessageBox.Show("Kindly provide your password", "Login Failed");
-                txtPassword.Focus();
-                return;
-            }
+                return LoginAuth.NOPASSWORD;
 
             var db = new AppDbContext();
             // Search for user using their employee id and their password
             var resultSet = db.Users.Where(user => (user.EmpID == employeeId) && (user.Password == password));
-            if(resultSet.Count() == 0 )
-            {
-                MaterialMessageBox.Show("Invalid login credentials", "Login Failed");
-                // The called function currently only clears the password
-                ResetFields();
-                return;
-            }
-            MaterialMessageBox.Show("Welcome", "Login Passed");
+            if (resultSet.Count() == 0)
+                return LoginAuth.AUTH_FAIL;
+            return LoginAuth.AUTH_PASS;
         }
+
+        public bool TestUserProvidedData(string userInput) => UserProvidedData(userInput);
 
         private bool UserProvidedData(string userInput)
         {
