b56e14e9b788560ccdf2c6735f7b13bb2a98dad6
Martin Amankwaa <martyofmca@gmail.coms>
2023-01-16 01:25:02 +0000

    refactor: add Enums.cs for all flags in codebase
    add the Enums.cs in the FPIS.Utils namespace to store all flags for the
    various actions we might perform. This is extremely helpful in situations
    where we might want to perform different actions based on the feedback
    obtained from a function call. For example, during authentication we
    might have to display a message when no employee id is provided. Or
    when a password is not provided. Or when the authentication fails or
    passes.

---
+++ FPIS/Utils/Enums.cs
@@ -0,0 +1,23 @@
+﻿namespace FPIS.Utils
+{
+    // NOTE: This Enums source code file can be used to store all the flags we will use in this codebase
+    internal enum LoginAuthenticationFlags
+    {
+        /// <summary>
+        /// User didn't provide a username
+        /// </summary>
+        NOUSERNAME,
+        /// <summary>
+        /// User didn't provide any password
+        /// </summary>
+        NOPASSWORD,
+        /// <summary>
+        /// User's login credentials not found
+        /// </summary>
+        AUTH_FAIL,
+        /// <summary>
+        /// User successfully connected to the system
+        /// </summary>
+        AUTH_PASS
+    }
+}
