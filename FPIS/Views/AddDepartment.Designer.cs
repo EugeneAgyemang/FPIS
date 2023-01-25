94f7fd8cfdea3c9a937455682a2a1568cc95f3bf
EugeneAgyemang <eugeneagyemang.ea@gmail.com>
2023-01-25 17:59:08 +0000

    Merge Conflict Resolved

---
+++ FPIS/Views/AddDepartment.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace FPIS.Views
+{
+    partial class AddDepartment
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAddDepartment = new MaterialSkin.Controls.MaterialButton();
+            this.materialTextBoxDepartmentName = new MaterialSkin.Controls.MaterialTextBox();
+            this.labelDepartmentName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAddDepartment
+            // 
+            this.btnAddDepartment.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnAddDepartment.CharacterCasing = MaterialSkin.Controls.MaterialButton.CharacterCasingEnum.Normal;
+            this.btnAddDepartment.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.btnAddDepartment.Depth = 0;
+            this.btnAddDepartment.HighEmphasis = true;
+            this.btnAddDepartment.Icon = null;
+            this.btnAddDepartment.Location = new System.Drawing.Point(137, 210);
+            this.btnAddDepartment.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnAddDepartment.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnAddDepartment.Name = "btnAddDepartment";
+            this.btnAddDepartment.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.btnAddDepartment.Size = new System.Drawing.Size(150, 36);
+            this.btnAddDepartment.TabIndex = 31;
+            this.btnAddDepartment.Text = "Add Department";
+            this.btnAddDepartment.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.btnAddDepartment.UseAccentColor = false;
+            this.btnAddDepartment.UseVisualStyleBackColor = true;
+            this.btnAddDepartment.Click += new System.EventHandler(this.btnAddDepartment_Click);
+            // 
+            // materialTextBoxDepartmentName
+            // 
+            this.materialTextBoxDepartmentName.AnimateReadOnly = false;
+            this.materialTextBoxDepartmentName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.materialTextBoxDepartmentName.Depth = 0;
+            this.materialTextBoxDepartmentName.Font = new System.Drawing.Font("Roboto", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.materialTextBoxDepartmentName.Hint = "Department Name";
+            this.materialTextBoxDepartmentName.LeadingIcon = null;
+            this.materialTextBoxDepartmentName.Location = new System.Drawing.Point(23, 98);
+            this.materialTextBoxDepartmentName.MaxLength = 50;
+            this.materialTextBoxDepartmentName.MouseState = MaterialSkin.MouseState.OUT;
+            this.materialTextBoxDepartmentName.Multiline = false;
+            this.materialTextBoxDepartmentName.Name = "materialTextBoxDepartmentName";
+            this.materialTextBoxDepartmentName.Size = new System.Drawing.Size(404, 50);
+            this.materialTextBoxDepartmentName.TabIndex = 32;
+            this.materialTextBoxDepartmentName.Text = "";
+            this.materialTextBoxDepartmentName.TrailingIcon = null;
+            // 
+            // labelDepartmentName
+            // 
+            this.labelDepartmentName.AutoSize = true;
+            this.labelDepartmentName.Location = new System.Drawing.Point(22, 157);
+            this.labelDepartmentName.Name = "labelDepartmentName";
+            this.labelDepartmentName.Size = new System.Drawing.Size(162, 20);
+            this.labelDepartmentName.TabIndex = 33;
+            this.labelDepartmentName.Text = "labelDepartmentName";
+            // 
+            // AddDepartment
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 274);
+            this.Controls.Add(this.labelDepartmentName);
+            this.Controls.Add(this.materialTextBoxDepartmentName);
+            this.Controls.Add(this.btnAddDepartment);
+            this.Name = "AddDepartment";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Add New Department";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private MaterialSkin.Controls.MaterialButton btnAddDepartment;
+        private MaterialSkin.Controls.MaterialTextBox materialTextBoxDepartmentName;
+        private Label labelDepartmentName;
+    }
+}
---
+++ FPIS/Views/AddDepartment.cs
@@ -0,0 +1,133 @@
+﻿using FPIS.Models;
+using MaterialSkin.Controls;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static MaterialSkin.MaterialSkinManager;
+
+namespace FPIS.Views
+{
+    public partial class AddDepartment : MaterialForm
+    {
+        bool _isDataValid = true;
+        public AddDepartment()
+        {
+            InitializeComponent();
+            Theme.FormInstance = this;
+            Theme.Set(Themes.LIGHT);
+            labelDepartmentName.Text = "";
+        }
+        private void ClearErrorLabels()
+        {
+            labelDepartmentName.Text = "";
+            
+        }
+        private void ValidateDepartmentName()
+        {
+
+            // 3 characters or more, no numbers and special characters
+            string nameRegexPattern = "^[ a-zA-Z]{3,}$";
+
+            string departmentName = materialTextBoxDepartmentName.Text.Trim();
+
+            if (departmentName.Length == 0)
+            {
+               labelDepartmentName.ForeColor = System.Drawing.Color.Red;
+               labelDepartmentName.Text = "Department name is required!";
+                _isDataValid = false;
+                return;
+            }
+
+            AppDbContext dbContext = new();
+            var department = dbContext.Departments.FirstOrDefault(department => department.DepartmentName == departmentName);
+            dbContext.Dispose();
+
+            if (department != null)
+            {
+                _isDataValid = false;
+                labelDepartmentName.ForeColor = System.Drawing.Color.Red;
+                labelDepartmentName.Text = "Department already Created.";
+                return;
+            }
+
+            Regex regex = new(nameRegexPattern);
+
+            if (!regex.IsMatch(departmentName))
+            {
+                labelDepartmentName.ForeColor = System.Drawing.Color.Red;
+                labelDepartmentName.Text = "Department name cannot have numbers.";
+                _isDataValid = false;
+                return;
+            }
+        }
+        private void ValidateInputs()
+        {
+            ValidateDepartmentName();
+         
+        }
+
+        private void CreateDepartment()
+        {
+            btnAddDepartment.Enabled = false;
+
+            Department department = new()
+            {
+                DepartmentName = materialTextBoxDepartmentName.Text, 
+            };
+            AppDbContext dbContext = new();
+            try
+            {
+                
+                dbContext.Departments.Add(department);
+                dbContext.SaveChanges();
+
+                MessageBox.Show(
+                    $"{department.DepartmentName} Department created successfully.",
+                    "Success",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information
+                );
+                
+                Console.WriteLine($"Create department {department}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error creating Department: {ex}");
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                btnAddDepartment.Enabled = true;
+                materialTextBoxDepartmentName.Text = "";
+                dbContext.Dispose();
+            }
+        }
+        private void btnAddDepartment_Click(object sender, EventArgs e)
+        {
+            _isDataValid = true;
+
+            ClearErrorLabels();
+            ValidateInputs();
+
+            if (!_isDataValid)
+            {
+                MessageBox.Show(
+                    "You have some invalid inputs",
+                    "Invalid Input",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+                return;
+            }
+            CreateDepartment();
+        }
+    }
+}
