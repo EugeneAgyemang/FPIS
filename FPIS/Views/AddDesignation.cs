50a706b57f3e8a1ffb5202b81cd53b89d543716e
EugeneAgyemang <eugeneagyemang.ea@gmail.com>
2023-01-25 23:50:24 +0000

    Review Comment Resolved

---
+++ FPIS/Views/AddDesignation.cs
@@ -0,0 +1,184 @@
+﻿using FPIS.Models;
+using MaterialSkin.Controls;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static MaterialSkin.MaterialSkinManager;
+
+namespace FPIS.Views
+{
+    public partial class AddDesignation : MaterialForm
+    {
+
+        bool _isDataValid = true;
+        string designationName;
+        public AddDesignation()
+        {
+            InitializeComponent();
+            Theme.FormInstance = this;
+            Theme.Set(Themes.LIGHT);
+            labelDepartmentName.Text = "";
+            labelDesignation.Text = "";
+       
+        }
+        private void ClearErrorLabels()
+        {
+            labelDepartmentName.Text = "";
+            labelDesignation.Text = "";
+
+        }
+        private void ValidateDesignationName()
+        {
+            // 3 characters or more, no numbers and special characters
+            string nameRegexPattern = "^[ a-zA-Z]{3,}$";
+
+            designationName = materialTextBoxDesignationName.Text.Trim();
+
+            if (designationName.Length == 0)
+            {
+                labelDesignation.ForeColor = System.Drawing.Color.Red;
+                labelDesignation.Text = "Designation name is required!";
+                _isDataValid = false;
+                return;
+            }
+            CheckIfDesignationExist(designationName);
+            Regex regex = new(nameRegexPattern);
+
+            if (!regex.IsMatch(designationName))
+            {
+                labelDesignation.ForeColor = System.Drawing.Color.Red;
+                labelDesignation.Text = "Designation name cannot have numbers.";
+                _isDataValid = false;
+                return;
+            }
+        }
+        private void CheckIfDesignationExist(string designationName)
+        {
+            AppDbContext dbContext = new();
+            var designation = dbContext.Designations.FirstOrDefault(designation => designation.DesignationName == designationName);
+            dbContext.Dispose();
+
+            if (designation != null)
+            {
+                _isDataValid = false;
+                labelDesignation.ForeColor = System.Drawing.Color.Red;
+                labelDesignation.Text = "Designation already Created.";
+                return;
+            }
+        }
+        private void ValidateInputs()
+        {
+            ValidateDesignationName();
+
+        }
+        private void LoadDeparments()
+        {
+            try
+            {
+                AppDbContext dbContext = new();
+                var departmentName = from Department in dbContext.Departments
+                                     select Department;
+                materialComboBoxDepartmentName.DataSource = departmentName.ToList();
+                materialComboBoxDepartmentName.DisplayMember = "DepartmentName";
+                dbContext.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error Loading Departments: {ex}");
+                MaterialMessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void AddDesignation_Load(object sender, EventArgs e)
+        {
+           LoadDeparments();
+        }   
+        private void CreateDesignation()
+        {
+            btnAddDesignation.Enabled = false;
+            AppDbContext dbContext = new();
+            var department = dbContext.Departments.Where(dpt => dpt.DepartmentName == materialComboBoxDepartmentName.Text).Single();
+            
+            Designation designation = new()
+            {
+                
+                DesignationName = materialTextBoxDesignationName.Text,
+                DepartmentId = department.Id,
+
+            };
+            try
+            {
+                dbContext.Designations.Add(designation);
+                dbContext.SaveChanges();
+
+                MessageBox.Show(
+                    $"\"{designation.DesignationName}\" Designation created successfully under {materialComboBoxDepartmentName.Text} Department.",
+                    "Success",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information
+                );
+
+                Console.WriteLine($"Create designation {designation}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error creating Department: {ex}");
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                btnAddDesignation.Enabled = true;
+                materialTextBoxDesignationName.Text = "";
+                dbContext.Dispose();
+            }
+        }
+
+        private void ConfirmNewDesignation()
+        {
+            DialogResult dialogResult = MaterialMessageBox.Show(
+                $"Do you want to create \"{designationName}\" as a new Designation?",
+                "Confirm",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question
+                );
+
+            if (dialogResult == DialogResult.Yes)
+            {
+                CreateDesignation();
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                materialTextBoxDesignationName.Text = "";
+            }
+        }
+
+        private void btnAddDesignation_Click(object sender, EventArgs e)
+        {
+            _isDataValid = true;
+
+            ClearErrorLabels();
+            ValidateInputs();
+
+            if (!_isDataValid)
+            {
+                MessageBox.Show(
+                    "You have some invalid inputs",
+                    "Invalid Input",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+                return;
+            }
+            ConfirmNewDesignation();
+        }
+      
+    }
+}
