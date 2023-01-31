4ce79e6e130b64f27f23c6665055480446744b6f
Patrick Denkyi <official.denkyipatrick@gmail.com>
2023-01-31 22:04:50 +0000

    feat: Add 'AnalysisProduct' as an 'AnalysisItem'.

---
+++ FPIS/Services/AnalysisItemService.cs
@@ -0,0 +1,45 @@
+﻿using FPIS.Models;
+using System.Text;
+namespace FPIS.Services
+{
+    public class AnalysisItemService
+    {
+        private readonly AppDbContext _dbContext;
+
+        /// <summary>
+        /// Constructor initialize a new `AnalysisItemService` with a DbContext
+        /// </summary>
+        /// <param name="dbContext"></param>
+        public AnalysisItemService(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public bool IsProductAnalysisProduct(Product product)
+        {
+            AnalysisProduct? analysisProduct = _dbContext.AnalysisProducts.FirstOrDefault(
+                analysisProduct => analysisProduct.ProductId == product.Id
+                );
+
+            if (analysisProduct == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public AnalysisProduct CreateAnalysisProduct(Product product)
+        {
+            var analysisItem = _dbContext.AnalysisItems.Add(new AnalysisItem() { ItemType = "Product" }).Entity;
+            var analysisProduct = _dbContext.AnalysisProducts.Add(new AnalysisProduct()
+            {
+                ProductId = product.Id,
+                AnalysisItemId = analysisItem.Id
+            }).Entity;
+
+            _dbContext.SaveChanges();
+            return analysisProduct;
+        }
+    }
+}
---
+++ FPIS/Views/CreateAnalysisProduct.cs
@@ -0,0 +1,67 @@
+﻿
+using FPIS.Services;
+using FPIS.Models;
+using MaterialSkin.Controls;
+using FPIS.Utils;
+
+namespace FPIS.Views
+{
+    public partial class CreateAnalysisProduct : Form
+    {
+        private readonly ProductService _productService;
+        private readonly AnalysisItemService _analysisItemService;
+
+        public CreateAnalysisProduct()
+        {
+            InitializeComponent();
+
+            AppDbContext dbContext = new();
+            _productService = new(dbContext);
+            _analysisItemService = new(dbContext);
+
+            LoadProducts();
+        }
+
+        private void LoadProducts()
+        {
+            materialComboBoxProducts.DataSource = _productService.GetAllProducts();
+            materialComboBoxProducts.DisplayMember = "ProductName";
+        }
+
+        private void materialButtonAddProduct_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string productName = materialComboBoxProducts.Text;
+                Product? product = _productService.GetProductByName(productName);
+
+                if (product == null)
+                {
+                    return;
+                }
+
+                if (_analysisItemService.IsProductAnalysisProduct(product))
+                {
+                    MessageBox.Show(
+                        $"\"{productName}\" is already an analysis item",
+                        "Success", MessageBoxButtons.OK,
+                        MessageBoxIcon.Error
+                        );
+
+                    return;
+                }
+
+                _analysisItemService.CreateAnalysisProduct(product);
+                MessageBox.Show(
+                    $"\"{productName}\" is successfully added as Analysis Item",
+                    "Success", MessageBoxButtons.OK,
+                    MessageBoxIcon.Information
+                    );
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error ${ex}");
+            }
+        }
+    }
+}
