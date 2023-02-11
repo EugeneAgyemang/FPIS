e235bb724357f199dab2408ca2c3b538e577b230
EugeneAgyemang <eugeneagyemang.ea@gmail.com>
2023-02-11 13:12:20 +0000

    Revert "Merge conflict resolved"
    This reverts commit 9586a8bb1fde83c55736c99819c22268017b3aea.

---
+++ test/FPIS_TEST/LoginTest.cs
@@ -0,0 +1,32 @@
+﻿using FPIS;
+using FPIS.Models;
+using FPIS.Utils;
+
+namespace FPIS_TEST
+{
+    [TestClass]
+    public class LoginTest
+    {
+        Login obj;
+        public LoginTest()
+        {
+            obj = new Login();
+        }
+        [TestMethod]
+        public void TestTestUserProvidedData()
+        {
+            string userInput = "Kofi";
+           // bool testResultIsTrue = obj.TestUserProvidedData(userInput);
+           // Assert.IsTrue(testResultIsTrue);
+        }
+
+        [TestMethod]
+        public void TestAuthenticateUser()
+        {
+            string employeeId = "1";
+            string password = Utils.HashPassword("savage");
+          //  LoginAuth authToken = obj.TestAuthenticateUser(employeeId, password);
+           // Assert.AreEqual(authToken, LoginAuth.AUTH_PASS);
+        }
+    }
+}
