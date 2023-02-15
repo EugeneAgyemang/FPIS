aa807f54786b289d9f49c43f1fe6ca5e81b24755
Martin Amankwaa <martyofmca@gmail.com>
2023-02-15 13:36:17 +0000

    fix: add a new migration for the recent change

---
+++ FPIS/Migrations/20230215133158_RemoveItemTypeFromProductParameterModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FPIS.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveItemTypeFromProductParameterModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ItemType",
+                table: "ProductParameters");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ItemType",
+                table: "ProductParameters",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
