d3b0550e76666e512e3961efa1d031e56b09cbb9
Martin Amankwaa <martyofmca@gmail.com>
2023-02-20 00:09:31 +0000

    add temporary files to aid User Control design

---
+++ FPIS/Views/TestUserControls.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FPIS.Views
+{
+    public partial class TestUserControls : Form
+    {
+        public TestUserControls()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Wipe the main panel so we add just what the user needs to see
+            MainPanelControl.Controls.Clear();
+
+            // then
+            UserControlB instance = new UserControlB();
+            MainPanelControl.Controls.Add(instance);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Would fail
+            // UserControlA instance = new UserControlA();
+
+            // Wipe the main panel so we add just what the user needs to see
+            MainPanelControl.Controls.Clear();
+
+            // then
+            UserControlA instance = UserControlA.Instance;
+            MainPanelControl.Controls.Add(instance);
+            // Or MainPanelControl.Controls.Add(UserControlA.Instance);
+        }
+    }
+}
---
+++ FPIS/Views/UserControlB.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FPIS.Views
+{
+    public partial class UserControlB : UserControl
+    {
+        public UserControlB()
+        {
+            InitializeComponent();
+        }
+
+        private void ActionButtonControl_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(TypeSomethingControl.Text, "The Wrong Pattern Right?", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
