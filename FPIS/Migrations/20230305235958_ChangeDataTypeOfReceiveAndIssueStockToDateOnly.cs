de987ed64516de34a5bc11e7839b4ed191aceb1d
EugeneAgyemang <eugeneagyemang.ea@gmail.com>
2023-03-06 00:35:16 +0000

    Receive and Issue Stock Items

---
+++ FPIS/Migrations/20230305235958_ChangeDataTypeOfReceiveAndIssueStockToDateOnly.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FPIS.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeDataTypeOfReceiveAndIssueStockToDateOnly : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "ReceivedDate",
+                table: "ReceivedStocks",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "IssuedDate",
+                table: "IssuedStocks",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ReceivedDate",
+                table: "ReceivedStocks",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "IssuedDate",
+                table: "IssuedStocks",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+    }
+}
