feb4ee34ae32ac6c07ac1dc1df6b5bc975383a68
Martin Amankwaa <martyofmca@gmail.com>
2023-04-21 00:18:12 +0000

    fix: data retrieved for various users on their dashboard

--- FPIS/Views/UserControlDashboard.cs
+++ FPIS/Views/UserControlDashboard.cs
@@ -21,13 +21,15 @@ namespace FPIS.Views
         AppDbContext appDbContext = new AppDbContext();
         int pendingItemXAxis = 25, pendingItemYAxis = 20;
         int completedItemXAxis = 25, completedItemYAxis = 20;
+        Guid userId;
         public UserControlDashboard()
         {
             InitializeComponent();
+            userId = Guid.Parse(Main.LOGGED_USER_ID);
             LoadUsers();
-            LoadSamples();
-            LoadSampleResults();
-            LoadPendingSamples();
+            SamplesControl.Text = $"{LoadSamples()}";
+            SampleResultsControl.Text = $"{LoadSamples("completed")}";
+            SamplesPendingControl.Text = $"{LoadSamples("pending")}";
 
             // User's live feed
             LiveLoadSamples("pending", SampleRequestedBreakdownDetailsSection, ref pendingItemXAxis, ref pendingItemYAxis);
@@ -36,25 +38,38 @@ namespace FPIS.Views
         }
         private void LoadUsers()
         {
-            RegisteredUsersControl.Text = $"{appDbContext.Users.Count()}";
-        }
-        private void LoadSamples()
-        {
-            SamplesControl.Text = $"{appDbContext.Samples.Count()}";
-        }
-        private void LoadSampleResults()
-        {
-            SampleResultsControl.Text = $"{appDbContext.SampleResults.Count()}"; ;
+            string fullName = new UserService(new()).GetFullName(userId);
+            if (fullName.ToLower().Trim() == "super admin")
+            {
+                RegisteredUsersControl.Text = $"{appDbContext.Users.Count()}";
+                return;
+            }
+            ActiveUsersSection.Visible = false;
+            SectionForUser.Location = new Point(87, 54);
         }
 
-        private void LoadPendingSamples()
+        private int LoadSamples(string status = "")
         {
-            SamplesPendingControl.Text = $"{appDbContext.Samples.Count() - appDbContext.SampleResults.Count()}"; ;
+            IQueryable<Sample> sampleQuery = appDbContext.Samples;
+            string fullName = new UserService(new()).GetFullName(userId);
+            if (fullName.ToLower().Trim() == "super admin")
+            {
+                if (status == string.Empty)
+                {
+                    return sampleQuery.Count();
+                }
+                return sampleQuery.Where(sample => sample.Status.ToLower() == status).Count();
+            }
+            if (status == string.Empty)
+            {
+                // Fetch samples which are both Pending and Completed (all samples) for a user
+                return sampleQuery.Where(sample => sample.UserId == userId && sample.Status.ToLower() != status).Count();
+            }
+            return sampleQuery.Where(sample => sample.UserId == userId && sample.Status.ToLower() == status).Count();
         }
 
         private void LiveLoadSamples(string status, Panel section, ref int xAxis, ref int yAxis)
         {
-            Guid userId = Guid.Parse(Main.LOGGED_USER_ID);
             LiveLoadProductSamples(userId, status, section, ref xAxis, ref yAxis);
             LiveLoadWaterSamples(userId, status, section, ref xAxis, ref yAxis);
         }
@@ -123,12 +138,20 @@ namespace FPIS.Views
 
         private IQueryable<SampleDetail> LiveLoadItemSamples(Guid userId, string status, string typeForFiltering)
         {
-            return appDbContext.SampleDetails
+            IQueryable<SampleDetail> sampleDetailsQuery = appDbContext.SampleDetails
                                     .Include(sampleDetail => sampleDetail.Sample)
-                                    .Where(sampleDetail => sampleDetail.Sample.UserId == userId &&
-                                                            sampleDetail.Sample.Status.ToLower() == status &&
+                                    .Where(sampleDetail => sampleDetail.Sample.Status.ToLower() == status &&
                                                             sampleDetail.Sample.TypeForFiltering.ToLower() == typeForFiltering &&
                                                             sampleDetail.Sample.Date == DateOnly.FromDateTime(DateTime.Now));
+
+            string fullName = new UserService(new()).GetFullName(userId);
+            if (fullName.ToLower().Trim() == "super admin")
+            {
+                return sampleDetailsQuery;
+            }
+            sampleDetailsQuery = sampleDetailsQuery.Where(sampleDetail => sampleDetail.Sample.UserId == userId);
+
+            return sampleDetailsQuery;
         }
 
         private MaterialLabel AddItem(string productName, int productCount, ref int itemXAxis, ref int itemYAxis)
@@ -169,6 +192,7 @@ namespace FPIS.Views
 
         private void asyncLoader_Tick(object sender, EventArgs e)
         {
+            userId = Guid.Parse(Main.LOGGED_USER_ID);
             pendingItemXAxis = completedItemXAxis = 25;
             pendingItemYAxis = completedItemYAxis = 20;
             LiveLoadSamples("pending", SampleRequestedBreakdownDetailsSection, ref pendingItemXAxis, ref pendingItemYAxis);
