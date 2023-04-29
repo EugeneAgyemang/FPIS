e509b90509d7cd6d41b39623ee72c70ed0866efe
Martin Amankwaa <104218489+MartyOfMCA@users.noreply.github.com>
2023-04-29 15:57:52 +0100

    Add AnalysisResultDataBindingItem

---
+++ AnalysisResultDataBindingItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace FPIS.Models
+{
+    public class AnalysisResultDataBindingItem
+    {
+        public string SampleResultId { get; set; }
+
+        public string AnalysisType { get; set; }
+
+        public string Name { get; set; }
+
+        public string ResultDate { get; set; }
+
+        public string Sample { get; set; }
+
+        public string ProductionEngineerOne { get; set; }
+
+        public string ProductionEngineerTwo { get; set; }
+
+        public string Update { get; set; } = "Update Result";
+    }
+
+}
