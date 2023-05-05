0661360c45b0f79e04b54e317397b0fdcc3ee3e8
Martin Amankwaa <martyofmca@gmail.com>
2023-05-05 01:36:44 +0100

    fix: store connection details in App.config

--- FPIS/Models/AppDbContext.cs
+++ FPIS/Models/AppDbContext.cs
@@ -7,8 +7,8 @@ namespace FPIS.Models
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            string password = ConfigurationManager.AppSettings["database_password"];
-            optionsBuilder.UseNpgsql($"Host=localhost;Port=5432;Database=FPIS;Username=postgres;Password={password}");
+            string connectionString = ConfigurationManager.AppSettings["connection"];
+            optionsBuilder.UseNpgsql(connectionString);
         }
 
 
