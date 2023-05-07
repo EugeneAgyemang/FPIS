4dd77b99fa3d03b524c9a5d5470ba0bccda5eacb
Martin Amankwaa <martyofmca@gmail.com>
2023-05-07 17:16:49 +0100

    feat: add new migrations for Material Procurement department

---
+++ FPIS/Migrations/20230507155119_AddWeightAndLocationFieldsToReceivingModel.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FPIS.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWeightAndLocationFieldsToReceivingModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Receivings",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Receivings",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "GrossWeight",
+                table: "Receivings",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "NetWeight",
+                table: "Receivings",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Receivings");
+
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Receivings");
+
+            migrationBuilder.DropColumn(
+                name: "GrossWeight",
+                table: "Receivings");
+
+            migrationBuilder.DropColumn(
+                name: "NetWeight",
+                table: "Receivings");
+        }
+    }
+}
