b9adbb673215bd290d571aba0f1cfa5a4e9a2d2a
Patrick Denkyi <official.denkyipatrick@gmail.com>
2023-05-22 21:12:22 +0000

    add reject column migration files

---
+++ FPIS/Migrations/20230517154335_AddIsRejectedColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FPIS.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsRejectedColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRejected",
+                table: "SampleResultDetails",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsRejected",
+                table: "SampleResultDetails");
+        }
+    }
+}
